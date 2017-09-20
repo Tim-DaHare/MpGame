3d296f7970ba0eb8ca3ce048bbcd35e01f6a916f
Tim-PC <tim.dahare@gmail.com>
2017-09-21 00:01:35 +0200

    car script added

---
+++ Assets/scripts/Controllers/CarController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    bool isControlled;
+    public GameObject[] passengers = new GameObject[4];
+
+    WheelCollider[] wheels;
+    Rigidbody rigidbody;
+
+    float enginePower = 150;
+
+    float power = 0;
+    float steer = 0;
+    float brake = 0;
+
+    float maxSteer = 25;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        wheels = GetComponentsInChildren<WheelCollider>();
+    }
+
+    private void Update()
+    {
+        if (!isControlled)
+        {
+            return;
+        }
+
+        power = Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0f;
+        steer = Input.GetAxis("Horizontal") * maxSteer;
+        brake = Input.GetKey("space") ? rigidbody.mass * 0.1f : 0.0f;
+
+        GetCollider(0).steerAngle = steer;
+        GetCollider(1).steerAngle = steer;
+
+        if (brake > 0.0)
+        {
+            GetCollider(0).brakeTorque = brake;
+            GetCollider(1).brakeTorque = brake;
+            GetCollider(2).brakeTorque = brake;
+            GetCollider(3).brakeTorque = brake;
+            GetCollider(2).motorTorque = 0.0f;
+            GetCollider(3).motorTorque = 0.0f;
+        }
+        else
+        {
+            GetCollider(0).brakeTorque = 0;
+            GetCollider(1).brakeTorque = 0;
+            GetCollider(2).brakeTorque = 0;
+            GetCollider(3).brakeTorque = 0;
+            GetCollider(2).motorTorque = power;
+            GetCollider(3).motorTorque = power;
+        }
+    }
+
+    WheelCollider GetCollider(int index)
+    {
+        return wheels[index];
+    }
+
+}
